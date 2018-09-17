50a4421da1d60bc75ff2d8334bfa9c05c33d83de
Arif Hossain <arif844@gmail.com>
2018-09-17 12:06:50 -0500

    accepted

---
+++ leetcode/src/mysql/problem183.sql
@@ -0,0 +1,6 @@
+# Suppose that a website contains two tables, the Customers (ID, Name) table and the Orders(ID, CustomerID) table.
+# Write a SQL query to find all customers who never order anything.
+
+
+
+
